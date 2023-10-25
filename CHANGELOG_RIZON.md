59a766b498b2a3385089c2629637011a6ba684b2
maoaixiao1314 <m18634441821@163.com>
2023-10-25 18:33:30 +0800

    treasurenet cosmosexplorer Necessary documents

---
+++ CHANGELOG_RIZON.md
@@ -0,0 +1,9 @@
+# rizon-v1.0.1 - 2021-02-25
+
+## Changes
+- merge `base-v2.0.0-rc3`
+
+# rizon-v1.0.0 - 2021-02-17
+
+## Changes
+- initial tag
---
+++ src/components/footer/utils.tsx
@@ -0,0 +1,74 @@
+// const FORBOLE_URL = 'https://www.forbole.com';
+const BIG_DIPPER_URL = 'https://bigdipper.live';
+const EVMEXPLORER_URL = 'https://evmexplorer.treasurenet.io';
+
+export const donateLink = {
+  key: 'donate',
+  url: `${BIG_DIPPER_URL}/donate`,
+};
+
+export const footerLinks = [
+  {
+    key: 'switch',
+    links: [
+      {
+        key: 'evmexplorer',
+        url: EVMEXPLORER_URL,
+      },
+      // {
+      //   key: 'stakeNow',
+      //   url: `${FORBOLE_URL}/stake-now`,
+      // },
+      // {
+      //   key: 'contact',
+      //   url: `${FORBOLE_URL}/contact`,
+      // },
+      // {
+      //   key: 'blog',
+      //   url: `${FORBOLE_URL}/blog`,
+      // },
+    ],
+  },
+  // {
+  //   key: 'company',
+  //   links: [
+  //     {
+  //       key: 'forbole',
+  //       url: FORBOLE_URL,
+  //     },
+  //     {
+  //       key: 'stakeNow',
+  //       url: `${FORBOLE_URL}/stake-now`,
+  //     },
+  //     {
+  //       key: 'contact',
+  //       url: `${FORBOLE_URL}/contact`,
+  //     },
+  //     {
+  //       key: 'blog',
+  //       url: `${FORBOLE_URL}/blog`,
+  //     },
+  //   ],
+  // },
+  // {
+  //   key: 'bigDipper',
+  //   links: [
+  //     {
+  //       key: 'about',
+  //       url: `${BIG_DIPPER_URL}/#about`,
+  //     },
+  //     {
+  //       key: 'faq',
+  //       url: `${BIG_DIPPER_URL}/faq`,
+  //     },
+  //     {
+  //       key: 'termsAndConditions',
+  //       url: `${BIG_DIPPER_URL}/terms-and-conditions`,
+  //     },
+  //     {
+  //       key: 'privacyPolicy',
+  //       url: `${BIG_DIPPER_URL}/privacy-policy`,
+  //     },
+  //   ],
+  // },
+];
